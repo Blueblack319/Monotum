62019d411e3e6b49eda885f9b94ddc78e442d080
Jae Hoon Yang <didwogns789@naver.com>
2020-08-12 22:47:27 +0900

    Register User

---
+++ src/js/name.js
@@ -0,0 +1,42 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greeting");
+
+const USER_LS = "currentUser",
+  SHOWING_ON = "showing";
+
+const saveUser = (text) => {
+  localStorage.setItem(USER_LS, text);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const userName = input.value;
+  paintGreeting(userName);
+  saveUser(userName);
+};
+
+const askForName = () => {
+  form.classList.add(SHOWING_ON);
+  form.addEventListener("submit", handleSubmit);
+};
+
+const paintGreeting = (text) => {
+  form.classList.remove(SHOWING_ON);
+  greeting.innerText = `Hello ${text}`;
+};
+
+const loadName = () => {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser) {
+    console.log(currentUser);
+    paintGreeting(currentUser);
+  } else {
+    askForName();
+  }
+};
+
+const init = () => {
+  loadName();
+};
+init();
