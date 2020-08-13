4083bd886b742937e41edf795b8a5c5056c96338
Jae Hoon Yang <didwogns789@naver.com>
2020-08-13 21:52:11 +0900

    Clock CSS

--- src/js/clock.js
+++ src/js/clock.js
@@ -4,10 +4,9 @@ const getTime = () => {
   const time = new Date();
   const hours = time.getHours();
   const minutes = time.getMinutes();
-  const seconds = time.getSeconds();
-  clock.innerText = `${hours >= 10 ? hours : `0${hours}`} : ${
+  clock.innerText = `${hours >= 10 ? hours : `0${hours}`}:${
     minutes >= 10 ? minutes : `0${minutes}`
-  } : ${seconds >= 10 ? seconds : `0${seconds}`}`;
+  }`;
 };
 
 const init = () => {
